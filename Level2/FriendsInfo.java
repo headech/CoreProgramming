88ce847bb9065cc4161e1d33c09dc694a17663f0
sparsh <sparshkathayat3256@gmail.com>
2025-08-12 00:05:57 +0530

    Code for Question-8

---
+++ Level2/FriendsInfo.java
@@ -0,0 +1,45 @@
+package Level_2;
+import java.util.Scanner;
+
+public class FriendsInfo {
+
+    public static String findYoungest(String[] names, int[] ages) {
+        int minIndex = 0;
+        for (int i = 1; i < ages.length; i++) {
+            if (ages[i] < ages[minIndex]) {
+                minIndex = i;
+            }
+        }
+        return names[minIndex];
+    }
+
+    public static String findTallest(String[] names, double[] heights) {
+        int maxIndex = 0;
+        for (int i = 1; i < heights.length; i++) {
+            if (heights[i] > heights[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        return names[maxIndex];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            ages[i] = sc.nextInt();
+            System.out.print("Enter height of " + names[i] + " (in cm): ");
+            heights[i] = sc.nextDouble();
+        }
+
+        String youngest = findYoungest(names, ages);
+        String tallest = findTallest(names, heights);
+
+        System.out.println("Youngest friend is: " + youngest);
+        System.out.println("Tallest friend is: " + tallest);
+    }
+}
