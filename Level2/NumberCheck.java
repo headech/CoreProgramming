a85edf33bd08d75cda137aa6aea6fcfb7698ebec
sparsh <sparshkathayat3256@gmail.com>
2025-08-12 00:08:01 +0530

    Code for Question-9

---
+++ Level2/NumberCheck.java
@@ -0,0 +1,55 @@
+package Level_2;
+import java.util.Scanner;
+
+public class NumberCheck {
+
+    public static boolean isPositive(int num) {
+        return num >= 0;
+    }
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static int compare(int num1, int num2) {
+        if (num1 > num2) {
+            return 1;
+        } else if (num1 == num2) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int num : numbers) {
+            if (isPositive(num)) {
+                System.out.print(num + " is Positive");
+                if (isEven(num)) {
+                    System.out.println(" and Even");
+                } else {
+                    System.out.println(" and Odd");
+                }
+            } else {
+                System.out.println(num + " is Negative");
+            }
+        }
+
+        int result = compare(numbers[0], numbers[numbers.length - 1]);
+        if (result == 0) {
+            System.out.println("First and last elements are equal.");
+        } else if (result == 1) {
+            System.out.println("First element is greater than the last element.");
+        } else {
+            System.out.println("First element is less than the last element.");
+        }
+    }
+}
