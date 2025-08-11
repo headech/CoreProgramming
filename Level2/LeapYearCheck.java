cd3068d25d0bfd3de6cd02aa1d7980bec626f30b
sparsh <sparshkathayat3256@gmail.com>
2025-08-11 23:58:32 +0530

    Code for Question-3

---
+++ Level2/LeapYearCheck.java
@@ -0,0 +1,25 @@
+package Level_2;
+import java.util.Scanner;
+
+public class LeapYearCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        if (year < 1582) {
+            System.out.println("Year should be >= 1582 (Gregorian calendar).");
+        } else {
+            if (isLeapYear(year)) {
+                System.out.println(year + " is a Leap Year.");
+            } else {
+                System.out.println(year + " is not a Leap Year.");
+            }
+        }
+        sc.close();
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
