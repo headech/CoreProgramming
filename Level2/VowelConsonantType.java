f8b8cce124ef11ff34f6526a54c7a84510b5c08a
sparsh <sparshkathayat3256@gmail.com>
2025-08-12 18:21:57 +0530

    Code for Question-6

---
+++ Level2/VowelConsonantType.java
@@ -0,0 +1,41 @@
+package Level_2;
+import java.util.Scanner;
+
+public class VowelConsonantType {
+    public static String checkCharType(char ch) {
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = (char) (ch + 32);
+        }
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return "Vowel";
+            } else {
+                return "Consonant";
+            }
+        }
+        return "Not a Letter";
+    }
+
+    public static String[][] classifyCharacters(String text) {
+        String[][] result = new String[text.length()][2];
+        for (int i = 0; i < text.length(); i++) {
+            result[i][0] = String.valueOf(text.charAt(i));
+            result[i][1] = checkCharType(text.charAt(i));
+        }
+        return result;
+    }
+
+    public static void displayTable(String[][] table) {
+        System.out.println("Character\tType");
+        for (int i = 0; i < table.length; i++) {
+            System.out.println(table[i][0] + "\t\t" + table[i][1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        String[][] result = classifyCharacters(text);
+        displayTable(result);
+    }
+}
