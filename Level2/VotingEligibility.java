41d74d26a4008e779efef3e318ca505cd539d12f
sparsh <sparshkathayat3256@gmail.com>
2025-08-12 18:24:01 +0530

    Code for Question-8

---
+++ Level2/VotingEligibility.java
@@ -0,0 +1,47 @@
+package Level_2;
+import java.util.Random;
+import java.util.Scanner;
+
+public class VotingEligibility {
+
+    public static int[] generateAges(int n) {
+        Random rand = new Random();
+        int[] ages = new int[n];
+        for (int i = 0; i < n; i++) {
+            ages[i] = rand.nextInt(90) + 10; // Random 2-digit age
+        }
+        return ages;
+    }
+
+    public static String[][] checkVotingEligibility(int[] ages) {
+        String[][] result = new String[ages.length][2];
+        for (int i = 0; i < ages.length; i++) {
+            result[i][0] = String.valueOf(ages[i]);
+            if (ages[i] < 0) {
+                result[i][1] = "false";
+            } else {
+                result[i][1] = (ages[i] >= 18) ? "true" : "false";
+            }
+        }
+        return result;
+    }
+
+    public static void displayTable(String[][] data) {
+        System.out.println("Age\tCan Vote");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i][0] + "\t" + data[i][1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int[] ages = generateAges(n);
+        String[][] eligibility = checkVotingEligibility(ages);
+
+        displayTable(eligibility);
+    }
+}
+
