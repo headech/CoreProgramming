75ab2478b91f55ca48b731b71e8b604a254fbd33
sparsh <sparshkathayat3256@gmail.com>
2025-08-12 18:18:08 +0530

    Code for Question-3

---
+++ Level2/WordLengthTable.java
@@ -0,0 +1,62 @@
+package Level_2;
+import java.util.Scanner;
+
+public class WordLengthTable {
+    public static int customLength(String text) {
+        int count = 0;
+        try {
+            while (true) {
+                text.charAt(count);
+                count++;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            return count;
+        }
+    }
+
+    public static String[] customSplit(String text) {
+        int len = customLength(text);
+        int spaceCount = 0;
+        for (int i = 0; i < len; i++) {
+            if (text.charAt(i) == ' ') spaceCount++;
+        }
+        int[] spaceIndexes = new int[spaceCount + 2];
+        spaceIndexes[0] = -1;
+        int index = 1;
+        for (int i = 0; i < len; i++) {
+            if (text.charAt(i) == ' ') {
+                spaceIndexes[index++] = i;
+            }
+        }
+        spaceIndexes[index] = len;
+        String[] words = new String[spaceCount + 1];
+        for (int i = 0; i < words.length; i++) {
+            String word = "";
+            for (int j = spaceIndexes[i] + 1; j < spaceIndexes[i + 1]; j++) {
+                word += text.charAt(j);
+            }
+            words[i] = word;
+        }
+        return words;
+    }
+
+    public static String[][] wordLengthTable(String[] words) {
+        String[][] table = new String[words.length][2];
+        for (int i = 0; i < words.length; i++) {
+            table[i][0] = words[i];
+            table[i][1] = String.valueOf(customLength(words[i]));
+        }
+        return table;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        String[] words = customSplit(text);
+        String[][] table = wordLengthTable(words);
+        System.out.println("Word\tLength");
+        for (int i = 0; i < table.length; i++) {
+            System.out.println(table[i][0] + "\t" + Integer.parseInt(table[i][1]));
+        }
+    }
+}
