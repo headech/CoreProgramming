bc7d6ec67e8f1f5c9ea5459da3e0792a85c6f6c0
sparsh <sparshkathayat3256@gmail.com>
2025-08-12 18:03:12 +0530

    Code for Question-5

---
+++ Level1/StringIndexDemo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class StringIndexDemo {
+    public static void generateException(String text) {
+        System.out.println(text.charAt(text.length()));
+    }
+
+    public static void handleException(String text) {
+        try {
+            System.out.println(text.charAt(text.length()));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("StringIndexOutOfBoundsException caught: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        //generateException(input);
+        handleException(input);
+    }
+}
+
