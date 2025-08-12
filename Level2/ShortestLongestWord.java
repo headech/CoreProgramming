a81eb100d107b193b7a5439982a9753e26881e61
sparsh <sparshkathayat3256@gmail.com>
2025-08-12 18:19:43 +0530

    Code for Question-4

---
+++ Level2/ShortestLongestWord.java
@@ -0,0 +1,75 @@
+package Level_2;
+import java.util.Scanner;
+
+public class ShortestLongestWord {
+    public static int customLength(String text) {
+        int count = 0;
+        try {
+            while (true) {
+                text.charAt(count);
+                count++;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            return count;
+        }
+    }
+
+    public static String[] customSplit(String text) {
+        int len = customLength(text);
+        int spaceCount = 0;
+        for (int i = 0; i < len; i++) {
+            if (text.charAt(i) == ' ') spaceCount++;
+        }
+        int[] spaceIndexes = new int[spaceCount + 2];
+        spaceIndexes[0] = -1;
+        int index = 1;
+        for (int i = 0; i < len; i++) {
+            if (text.charAt(i) == ' ') {
+                spaceIndexes[index++] = i;
+            }
+        }
+        spaceIndexes[index] = len;
+        String[] words = new String[spaceCount + 1];
+        for (int i = 0; i < words.length; i++) {
+            String word = "";
+            for (int j = spaceIndexes[i] + 1; j < spaceIndexes[i + 1]; j++) {
+                word += text.charAt(j);
+            }
+            words[i] = word;
+        }
+        return words;
+    }
+
+    public static String[][] wordLengthTable(String[] words) {
+        String[][] table = new String[words.length][2];
+        for (int i = 0; i < words.length; i++) {
+            table[i][0] = words[i];
+            table[i][1] = String.valueOf(customLength(words[i]));
+        }
+        return table;
+    }
+
+    public static int[] findShortestLongest(String[][] table) {
+        int shortestIndex = 0, longestIndex = 0;
+        for (int i = 1; i < table.length; i++) {
+            int len = Integer.parseInt(table[i][1]);
+            if (len < Integer.parseInt(table[shortestIndex][1])) {
+                shortestIndex = i;
+            }
+            if (len > Integer.parseInt(table[longestIndex][1])) {
+                longestIndex = i;
+            }
+        }
+        return new int[]{shortestIndex, longestIndex};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        String[] words = customSplit(text);
+        String[][] table = wordLengthTable(words);
+        int[] result = findShortestLongest(table);
+        System.out.println("Shortest word: " + table[result[0]][0] + " (Length: " + table[result[0]][1] + ")");
+        System.out.println("Longest word: " + table[result[1]][0] + " (Length: " + table[result[1]][1] + ")");
+    }
+}
