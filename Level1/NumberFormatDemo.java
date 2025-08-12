b67de5f070d63b9357527b2468ddd24b36c10320
sparsh <sparshkathayat3256@gmail.com>
2025-08-12 18:06:35 +0530

    Code for Question-7

---
+++ Level1/NumberFormatDemo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NumberFormatDemo {
+    public static void generateException(String text) {
+        int num = Integer.parseInt(text);
+        System.out.println(num);
+    }
+
+    public static void handleException(String text) {
+        try {
+            int num = Integer.parseInt(text);
+            System.out.println(num);
+        } catch (NumberFormatException e) {
+            System.out.println("NumberFormatException caught: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("RuntimeException caught: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        handleException(input);
+    }
+}
