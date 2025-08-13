de4cd2fd840bcb2646095c216da29175cc3c73a2
sparsh <sparshkathayat3256@gmail.com>
2025-08-13 10:20:55 +0530

    Code for question-8

---
+++ Extras/CompareStrings.java
@@ -0,0 +1,31 @@
+package String.Extras;
+import java.util.Scanner;
+
+public class CompareStrings {
+    public static int compareStrings(String str1, String str2) {
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int minLen = Math.min(len1, len2);
+        for (int i = 0; i < minLen; i++) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                return str1.charAt(i) - str2.charAt(i);
+            }
+        }
+        return len1 - len2;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+        int result = compareStrings(str1, str2);
+        if (result < 0)
+            System.out.println("\"" + str1 + "\" comes before \"" + str2 + "\" in lexicographical order");
+        else if (result > 0)
+            System.out.println("\"" + str1 + "\" comes after \"" + str2 + "\" in lexicographical order");
+        else
+            System.out.println("Both strings are equal");
+        sc.close();
+    }
+}
