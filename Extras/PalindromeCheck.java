4d96e7a4d491f6693ef7a08edc59b13dc129a728
sparsh <sparshkathayat3256@gmail.com>
2025-08-13 10:20:51 +0530

    Code for question-3

---
+++ Extras/PalindromeCheck.java
@@ -0,0 +1,27 @@
+package String.Extras;
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        if (isPalindrome(input))
+            System.out.println("The string is a palindrome.");
+        else
+            System.out.println("The string is not a palindrome.");
+        sc.close();
+    }
+}
+
