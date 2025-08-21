3dc9d841c53cc3e898ea161e30143358bf714896
sparsh <sparshkathayat3256@gmail.com>
2025-08-21 17:43:10 +0530

    Code for question-3

---
+++ Level1/KmToMiles.java
@@ -0,0 +1,7 @@
+public class KmToMiles {
+    public static void main(String[]args){
+        double distanceKm = 10.8;
+        double distanceMiles = distanceKm*1.6;
+        System.out.println("The distance "+distanceKm+" km in miles is "+distanceMiles);
+    }
+}
