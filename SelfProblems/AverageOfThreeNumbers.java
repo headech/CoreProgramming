9f24199eae7e8b06aa72a31b7e48ca34174b6b01
sparsh <sparshkathayat3256@gmail.com>
2025-08-27 11:02:45 +0530

    Code for question-4

---
+++ SelfProblems/AverageOfThreeNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class AverageOfThreeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        double average = (num1 + num2 + num3) / 3.0;
+        System.out.printf("The Average of Three Numbers is: %.2f\n", average);
+        sc.close();
+    }
+}
